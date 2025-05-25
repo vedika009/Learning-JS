bba123b223dc833577253e90a8008b3b88f5b704
Vedika Mishra <vedikamishra009@gmail.com>
2025-05-25 02:29:14 +0000

    do it

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true // NaN - not a number (type of NaN is number only)
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueNumber = Number(score)
-console.log(typeof(valueNumber))
-console.log(valueNumber) 
+// console.log(typeof(valueNumber))
+// console.log(valueNumber) 
 
 // when it converts to number
 // "33" - to number (33)
@@ -17,8 +17,8 @@ console.log(valueNumber)
 
 let isLoggedin = 1
 let booleanIsLoggedIn = Boolean(isLoggedin) 
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // when it converts to Boolean
 // "" (empty string) - false
@@ -27,5 +27,21 @@ console.log(booleanIsLoggedIn)
 
 let num = 33
 let strNum = String(num)
-console.log(strNum)
-console.log(typeof strNum)
+// console.log(strNum)
+// console.log(typeof strNum)
+
+// console.log(2 + "1")
+// console.log("1" + 2);
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "1");
+
+// console.log(+true); // to number - 1
+// console.log(+""); // to numbe - 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
