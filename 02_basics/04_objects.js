aee261580d1f662bb5ffea8012a75629aa22be2a
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-24 14:57:40 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,15 +1,27 @@
-//const tinderUser = new Object() - non singleton object
-const tinderUser = {} // - non singleton object
+//const tinderUser = new Object() //- non singleton object
+ const tinderUser = {} // - non singleton object
 
-tinderUser.id = "123abc"
-tinderUser.name = "Led"
-tinderUser.isLoggedin = false
+ tinderUser.id = "123abc"
+ tinderUser.name = "Led"
+ tinderUser.isLoggedin = false
  
-console.log(tinderUser)
+// console.log(tinderUser)
 
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
-        userfullname: 
+        userfullname: {
+            firstname: "Vedika",
+            lastname: "Mishra"
+            // access values through dot notation only
+        }
     }
 }
+
+console.log(regularUser.fullname.userfullname.lastname);
+// question mark after an object/value means what if it doesn't exist?, so instead of if-else it's used
+
+const obj1 = { 1: "a", 2: "b"}
+const obj2 = { 3: "a", 4: "b"}
+
+//const obj3 = { obj1, obj2} // this will put object in object
