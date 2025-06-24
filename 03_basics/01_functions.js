b65065c36c5e36988d87c296542746d5b3ce4691
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-24 18:45:55 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+function sayMyName() {
+    console.log("V");
+    console.log("E");
+    console.log("D");
+    console.log("I");
+    console.log("K");
+    console.log("A");
+}
+sayMyName() // prenthesis means executing it
