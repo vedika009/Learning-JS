fc7c6b1a85b0ad461b9a6480d5eea64c72481acc
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-28 07:44:03 +0000

    default parameter

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,3 +7,31 @@ function sayMyName() {
     console.log("A");
 }
 sayMyName() // prenthesis means executing it
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "Vedika"){ // default parameter, if nothing, then this will execute
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Vedika"))
+// console.log(loginUserMessage("Vedika"))
