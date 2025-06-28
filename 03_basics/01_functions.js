ae4b2e3110aac733d2d1a85afe0849648d7e2bc5
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-28 08:03:51 +0000

    scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,7 +6,7 @@ function sayMyName() {
     console.log("K");
     console.log("A");
 }
-sayMyName() // prenthesis means executing it
+//sayMyName() // prenthesis means executing it
 
 // function addTwoNumbers(number1, number2){
 
@@ -35,3 +35,44 @@ function loginUserMessage(username = "Vedika"){ // default parameter, if nothing
 
 // console.log(loginUserMessage("Vedika"))
 // console.log(loginUserMessage("Vedika"))
+
+function addCartPrice(...num1){// ... - rest (and spread operator) return type - array
+    return num1
+}
+
+function addCartPrice1(val1, val2, ...num1){// ... - rest (and spread operator) return type - array
+    //return num1
+    return val2
+}
+
+//console.log(addCartPrice(200, 100, 500, 2000));
+//console.log(addCartPrice1(200, 100, 500, 2000)); // first 2 values went in val1 and val2
+
+const user = {
+    username: "Vedika",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+//handleObject(user) // if it's wrong then it will print "undefined"
+
+// object bana ke bhi pass kr skte h, function call krte time
+handleObject(
+    user1 = {
+        username: "Vedika",
+        price: "CSE"
+    }
+)
+
+// passing arrays
+const myNewArr = [200, 78, 45, 34]
+
+function return2ndVal(anyArr){
+    return anyArr[1]
+}
+
+//console.log(return2ndVal(myNewArr))
+console.log(return2ndVal([45, 54, 34, 89]))
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,8 @@
+// closures are part of "scopes"
+let a = 10
+const b = 20
+var c = 30
+
+console.log(a)
+console.log(b)
+console.log(c)
