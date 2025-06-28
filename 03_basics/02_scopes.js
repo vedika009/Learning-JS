28a2bf8655f96aad16034ec525618ec0d85bf878
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-28 08:43:46 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,60 @@ let a = 300
     let a = 10 
     const b = 20
     var c = 30
-    console.log("Inner: ", a)
+    //console.log("Inner: ", a)
 }
 
-console.log(a) // not defined
+//console.log(a) // not defined
 // console.log(b) //same
 //console.log(c) // avoid varm scope ke bahar nahi jaana chaiye koi bhi value
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+// }
+
+// nest scope
+function one(){
+    const username = "vedika"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    console.log(website); //error
+    two()
 }
+
+//one()
+
+if(true) {
+    const username = "Vedika"
+    if(username === "Vedika") {
+        const website = " YouTube"
+        //console.log(username + website)
+    }
+    //console.log(website) // error
+}
+ 
+//console.log(username) // error
+
+// +++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++
+
+//declaring before defining
+console.log(addOne(6)) // no error
+
+function addOne(num) {
+    return num + 1
+}
+
+// addOne(6)
+
+//_____________________HOISTING_______________________
+
+//declaring before defining
+//console.log(addTwo(9)) // error
+
+const addTwo = function(num){ // an expression (kind of a fuction only, a bit different)
+    return num + 1
+}
+
+// addTwo(9)
