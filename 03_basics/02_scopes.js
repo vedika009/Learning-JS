f61f9373880e9de015113f713fe8dc6287d75399
Vedika Mishra <vedikamishra009@gmail.com>
2025-06-28 08:10:43 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,8 +1,17 @@
 // closures are part of "scopes"
-let a = 10
-const b = 20
-var c = 30
 
-console.log(a)
-console.log(b)
-console.log(c)
+let a = 300
+{ // this is a scope
+    let a = 10 
+    const b = 20
+    var c = 30
+    console.log("Inner: ", a)
+}
+
+console.log(a) // not defined
+// console.log(b) //same
+//console.log(c) // avoid varm scope ke bahar nahi jaana chaiye koi bhi value
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+}
