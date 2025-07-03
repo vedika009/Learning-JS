b2bf0f67b2fbd94ed47b9390ce3748a26811fc59
Vedika Mishra <vedikamishra009@gmail.com>
2025-07-03 10:02:58 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loops
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value is ${index}`);
+    index+=2
+}
+
+// for array
+let myArray = ['flash', 'batman', 'superman']
+
+let j = 0
+while(j < myArray.length) {
+    console.log(`Value is: ${myArray[j]}`);
+    j++
+}
+
+// do while loop
+console.log("_------_")
+let score = 11 // still it will run once 
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while(score <= 10)
