8f2fcb8c38f101a724d7c1ffe79e0b4bbd0dda7d
vedika009 <vedikamishra009@gmail.com>
2025-08-24 21:23:35 +0530

    oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -10,20 +10,54 @@ const user = {
     getUserDetails: function(){
         // console.log("got user details from database"); // undefined will also come, will discuss later
         // console.log(`Username: ${this.username}`);
-        console.log(this); // current context
+        //console.log(this); // current context
     }
 }
 
-console.log(user.username);
+//console.log(user.username);
 // console.log(user.getUserDetails());
-console.log(this); // empty - nothing in global context
+//console.log(this); // empty - nothing in global context
 
 // global context keeps changing
 
 // this keyword - to provide current context 
 
 // ------------------------------------------constructor function------------------------------------------------------
-const promiseOne = new Promise()
-const date = new Date()
+// const promiseOne = new Promise()
+// const date = new Date()
 
-// new keyword - constructor function
+// new keyword - constructor function 
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    // can make method also
+    this.greeting = function(){
+        //console.log(`Welcome ${this.username}`);
+    }
+
+    // return this
+}
+
+// const userOne = User("hitesh", 12, true) 
+// const userTwo = User("vedika", 34, false) // changed
+
+// so use new
+const userOne = new User("hitesh", 12, true) 
+const userTwo = new User("vedika", 34, false) // changed
+
+//console.log(userOne);
+//console.log(userTwo);
+console.log(userOne.constructor); // [Function: User] (khud ke baare mein reference)
+
+// ------new keyword
+/*
+
+- creates an empty object, called instance
+- constructor function is called, because of new keyword
+
+instanceof - is it the same keyword?
+
+*/
